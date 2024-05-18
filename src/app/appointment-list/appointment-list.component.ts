c2624272cced410ce8edec29eabc47ae6bb4b16c
ryoheiY <momoyamashia@icloud.com>
2024-05-19 00:53:42 +0900

    Layoutを修正！

--- src/app/appointment-list/appointment-list.component.ts
+++ src/app/appointment-list/appointment-list.component.ts
@@ -1,37 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Appointment } from '../models/appointment';
 
 @Component({
   selector: 'app-appointment-list',
   templateUrl: './appointment-list.component.html',
-  styleUrls: ['./appointment-list.component.css']
+  styleUrls: ['./appointment-list.component.css'],
 })
-export class AppointmentListComponent {
+export class AppointmentListComponent implements OnInit {
   count = 0;
-  newAppointmentTitle :string = "";
+  newAppointmentTitle: string = '';
   newAppointmentDate: Date | undefined = undefined;
 
-  appointments :Appointment[] = []; 
-
-  handleAdd(){
-
-    if(this.newAppointmentTitle.trim().length == 0 || !this.newAppointmentDate){
-      alert("No!")
+  appointments: Appointment[] = [];
+  ngOnInit(): void {
+    let savedAppointment = localStorage.getItem("appointments");
+    this.appointments = savedAppointment? JSON.parse(savedAppointment) : [];
+  }
+  handleAdd() {
+    if (
+      this.newAppointmentTitle.trim().length == 0 ||
+      !this.newAppointmentDate
+    ) {
+      alert('No!');
       return;
     }
 
-    alert("Input!!")
+    alert('Input!!');
     this.appointments.push({
-      id : ++this.count,
+      id: ++this.count,
       title: this.newAppointmentTitle,
-      date: this.newAppointmentDate
-    })
+      date: this.newAppointmentDate,
+    });
 
-    this.newAppointmentTitle = "";
+    this.newAppointmentTitle = '';
     this.newAppointmentDate = undefined;
+
+    localStorage.setItem('appointments', JSON.stringify(this.appointments));
   }
 
-  handleDelete(index: number){
+  handleDelete(index: number) {
     this.appointments.splice(index, 1);
+    localStorage.setItem('appointments', JSON.stringify(this.appointments));
   }
 }
